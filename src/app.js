f0e838f49c2be7fcdec83addc4a652a54009f3fb
Sebastian Avendaño Barrales <sebastian.avendano@borealis.cl>
2022-11-12 12:08:14 -0300

    feature: rest API CRUD

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express, { response } from 'express'
+import employeesRoutes from "./routes/employees.routes.js";
+import indexRoutes from "./routes/index.routes.js";
+import {PORT} from "./config.js";
+
+const app = express()
+
+app.use(express.json())
+
+app.use(indexRoutes)
+app.use('/api',employeesRoutes)
+
+app.use((req, res, next) => {
+    res.status(404).json({
+        mesagge: 'Endpoint not found'
+    })
+})
+
+export default app
